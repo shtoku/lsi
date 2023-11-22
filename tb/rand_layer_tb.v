5c68eddde6fa9a05a12859602ae0ced6fec02150
shtoku <23wm4342@student.gs.chiba-u.jp>
2023-11-22 10:47:38 +0900

    rand_layer.vを出力サンプルにより検証

--- tb/rand_layer_tb.v
+++ tb/rand_layer_tb.v
@@ -10,18 +10,35 @@ module rand_layer_tb ();
 
   rand_layer rand_layer_inst (.*);
 
+  genvar i;
+  reg  [`N_LEN-1:0] q_mem [0:`HID_DIM-1];
+  wire [`HID_DIM*`N_LEN-1:0] q_ans;
+  wire correct;
+
+  assign correct = (q == q_ans);
+
+  generate
+    for (i = 0; i < `HID_DIM; i = i + 1) begin
+      assign q_ans[i*`N_LEN +: `N_LEN] = q_mem[i];
+    end
+  endgenerate
+
   initial clk = 0;
   always #5 clk = ~clk;
+
+  initial begin
+    $readmemb("../data/tb/rand_layer_out_tb.txt", q_mem);
+  end
 
   initial begin
     $dumpvars;
     rst_n=0; run=0; #10
     rst_n=1; run=1; #10
     #300
-    run=0; #10
-    #50
-    run=1; #10
-    #300
+    // run=0; #10
+    // #50
+    // run=1; #10
+    // #300
     $finish;
   end
 
--- tools/kmj_gen_sim.py
+++ tools/kmj_gen_sim.py
@@ -161,22 +161,23 @@ if __name__ == '__main__':
   # 類似生成・新規生成テスト
   xors = xorshift.XorShift(5671)
   
-  for _ in range(10):
-    z = np.empty_like(o_mix2[:, 0])
-    for i in range(hid_dim):
-      # z[i] = kg.add(o_mix2[:, 0][i], xors())  # 類似生成
-      z[i] = xors()                           # 新規生成
+  z = np.empty_like(o_mix2[:, 0])
+  for i in range(hid_dim):
+    # z[i] = kg.add(o_mix2[:, 0][i], xors())  # 類似生成
+    z[i] = xors()                           # 新規生成
 
-    # mix_layer3
-    # 入力を複製
-    i_mix3 = np.full((hid_dim, hid_dim), z).T
-    o_mix3 = mix_layer(3, i_mix3)
+  output_file(z, TB_PATH + 'rand_layer_out_tb.txt')
 
-    # dense_layer
-    i_dens = o_mix3[:N, :]
-    o_dens = dense_layer(i_dens)
+  # mix_layer3
+  # 入力を複製
+  i_mix3 = np.full((hid_dim, hid_dim), z).T
+  o_mix3 = mix_layer(3, i_mix3)
 
-    # comp_layer
-    o_comp = comp_layer(o_dens)
+  # dense_layer
+  i_dens = o_mix3[:N, :]
+  o_dens = dense_layer(i_dens)
 
-    print('generate :', convert_str(o_comp))
+  # comp_layer
+  o_comp = comp_layer(o_dens)
+
+  print('generate :', convert_str(o_comp))
