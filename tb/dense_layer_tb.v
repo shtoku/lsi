d3db8ae6fe78374d1c09ab465c1fbc52a27ef7b7
kaitotsch <23WM4312@student.gs.chiba-u.jp>
2023-11-27 13:47:44 +0900

    dense_layer_tb.vの修正

--- tb/dense_layer_tb.v
+++ tb/dense_layer_tb.v
@@ -12,12 +12,21 @@ module dense_layer_tb ();
     dense_layer dense_layer_inst (.*);
 
     reg  [`N_LEN-1:0] d_mem [0:`N*`HID_DIM-1];
+    reg  [`N_LEN-1:0] q_mem [0:`N*`CHAR_NUM-1];
+    wire [`N*`CHAR_NUM*`N_LEN-1:0] q_ans;
+    wire correct;
+
+    assign correct = (q == q_ans);
 
     genvar i;
 
     generate
         for (i = 0; i < `N*`HID_DIM; i = i + 1) begin
             assign d[i*`N_LEN +: `N_LEN] = d_mem[i];
+        end
+
+        for (i =0; i < `N*`CHAR_NUM; i = i + 1) begin
+            assign q_ans[i*`N_LEN +: `N_LEN] = q_mem[i];
         end
     //     for (i = 0; i < `N*`EMB_DIM; i = i + 1) begin
     //         assign q_ans[i*`N_LEN +: `N_LEN] = q_mem[i];
@@ -26,7 +35,7 @@ module dense_layer_tb ();
 
     initial begin
         $readmemb("../data/tb/dense_layer_in_tb.txt", d_mem);
-        // $readmemb("../data/tb/emb_layer_out_tb.txt", q_mem);
+        $readmemb("../data/tb/dense_layer_out_tb.txt", q_mem);
     end
 
 
