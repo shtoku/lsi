3af22077c9b58b2d25976cdcf4532cd8d40082d8
shtoku <23wm4342@student.gs.chiba-u.jp>
2023-12-25 23:12:30 +0900

    新たなパラメータ用のファイルを作成 emb_dim, hid_dimを24から12に削減

--- tools/generate_train.py
+++ tools/generate_train.py
@@ -9,9 +9,9 @@ PATH_BIN192 = '../data/parameter/train/binary192/'
 
 
 N = 10              # 最大文字数
-char_num = 200      # 文字種数
-emb_dim = 24        # 文字ベクトルの次元
-hid_dim = 24        # 潜在ベクトルの次元
+char_num = 64       # 文字種数
+emb_dim = 12        # 文字ベクトルの次元
+hid_dim = 12        # 潜在ベクトルの次元
 
 
 # 固定小数点の桁数
